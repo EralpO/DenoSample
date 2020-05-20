70b029bd8c5cf6f499c6cf309a68f59441f22fec
EralpO <eralponay10@gmail.com>
2020-05-20 03:22:28 +0300

    initial commit

--- router.ts
+++ router.ts
@@ -1,5 +1,7 @@
 import {Router} from 'https://deno.land/x/oak/mod.ts'
 import products from './db/products.ts'
+import { Product } from './db/types.ts'
+import {v4} from 'https://deno.land/std/uuid/mod.ts'
 
 const router = new Router()
 
@@ -11,11 +13,43 @@ router.get('/api/products',({response }:{response:any} )=>{
  })
 
  //Get Single Product By Id
- router.get('/api/products/:id',({response }:{response:any} )=>{
-    response.body = {
-        success:true,
-        products}
+ router.get('/api/products/:id',async ({ params,response }:{params :{id:string} ,response:any }) =>{
+    const product : Product | undefined = await products.find(p => p.id === params.id)
+    if(product){
+        response.status = 200
+        response.body = {
+            success : true,
+            data : product
+        }
+    }
+    else {
+        response.body = {
+            msg : 'no Product found'
+        }
+    }
+    
  })
+
+ //Add a Product to Db
+   router.post('/api/products/add',async ({request,response}:{request:any,response:any})=>{
+       let body = await request.body();
+       if(request.hasBody){
+        let product  : Product = body.value
+        product.id = v4.generate()
+        products.push(product)
+        
+        response.body ={
+            success:true,
+            msg: product
+        }
+       }
+       else{
+           response.body = {
+               msg:'No data'
+           }
+       }
+
+   })
 
  
 
